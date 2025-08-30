449e37f23bf671d78a14e80bc2ddd5ec34640189
Cristian <cristianherrera01k@gmail.com>
2025-08-30 18:53:37 -0500

    editar y crear usuarios funcionando

--- front/src/components/dashboard/users/UserManagement.tsx
+++ front/src/components/dashboard/users/UserManagement.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { UsersPage } from './UsersPage';
+import { UsersList } from './UsersList';
+import { CreateUser } from './CreateUser';
 import { EditUser } from './EditUser';
 
 export const UserManagement: React.FC = () => {
   return (
     <Routes>
-      <Route index element={<UsersPage />} />
+      <Route index element={<UsersList />} />
+      <Route path="new" element={<CreateUser />} />
       <Route path=":id/edit" element={<EditUser />} />
       <Route path="*" element={<Navigate to="/dashboard/users" replace />} />
     </Routes>
